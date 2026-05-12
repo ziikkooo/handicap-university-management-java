bef3167c16a51a1d220bdbf27896bcbb4d5c50da
ziikkooo <lehdilizakaria1@gmail.com>
2026-05-12 23:28:25 +0100

    Empty starter files but keep project structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# handicap-university-management-java
-Java Desktop application for managing requests and complaints of disabled students in a university.
--- src/main/java/com/university/handicap/Main.java
+++
@@ -1,7 +0,0 @@
-package com.university.handicap;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Handicap University Management App started.");
-    }
-}
